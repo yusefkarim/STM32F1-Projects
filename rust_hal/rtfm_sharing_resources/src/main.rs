df26c3b2034f21aeb5a771954944c240e8da979c
Yusef Karim <yusefkarim@riseup.net>
2019-09-26 17:09:07 -0400

    Updated example to use cortex_m

--- rust_hal/rtfm_sharing_resources/src/main.rs
+++ rust_hal/rtfm_sharing_resources/src/main.rs
@@ -4,28 +4,23 @@
 extern crate panic_halt;
 
 // Rust core and external imports
-use core::{str, fmt::Write};
 use rtfm::app;
-use rtfm::Peripherals;
-use cortex_m::asm::delay;
-use embedded_hal::digital::v2::OutputPin;
-use stm32f1xx_hal::{
-    prelude::*,
-    delay::Delay,
-    device::TIM1,
-};
+// use rtfm::Peripherals;
+use stm32f1xx_hal::prelude::*;
+// use embedded_hal::digital::v2::OutputPin;
 // use core::convert::TryInto;
 
 // use cortex_m_semihosting::hprint;
 
 #[app(device = stm32f1xx_hal::device)]
 const APP: () = {
-    static mut CORE: Peripherals<'static> = ();
+    static mut CORE: cortex_m::Peripherals = ();
 
     #[init]
     fn init() -> init::LateResources {
         let mut flash = device.FLASH.constrain();
         let mut rcc = device.RCC.constrain();
+        let core_peripherals = cortex_m::Peripherals::take().unwrap();
         // let scb = core.SCB;
 
         let clocks = rcc
@@ -39,7 +34,7 @@ const APP: () = {
         // scb.system_reset();
         // CORE = core.try_into().unwrap();
         init::LateResources {
-            CORE: core,
+            CORE: core_peripherals,
         }
     }
 
